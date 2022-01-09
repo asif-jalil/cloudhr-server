d2856d092a754626f426d277ccc87f27a092bb62
Asif Jalil <62372647+asif-jalil@users.noreply.github.com>
2022-01-10 03:33:43 +0600

    fixed

--- src/app.js
+++ src/app.js
@@ -5,55 +5,6 @@ require("dotenv").config({ path: `${__dirname}/../.env` });
 const db = require("./db/db");
 const employeesRouter = require("./routes/employees");
 const app = express();
-// const fs = require("fs");
-
-// async function writeFile(path) {
-//   try {
-//     const writeStream = fs.createWriteStream(path, {
-//       flags: "w",
-//     });
-
-//     const letters = [
-//       "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-//     ];
-
-// 		for (let index = 0; index < 10000; index++) {
-// 			const fName = await makeRandomName(letters);
-// 			const lName = await makeRandomName(letters);
-//       writeStream.write(`${fName},${lName},email${index}@mail.com\n`, (err) => {
-//         if (err) throw err;
-//         if (index === 10000 - 1) writeStream.end(); //I attempted close() and destroy() too, none worked
-//       });
-//     }
-
-//     writeStream.on("finish", () => {
-//       console.log("All files were written."); //Currently being emmited before all is written.
-//     });
-//   } catch (err) {
-//     throw err;
-//   }
-// }
-
-// writeFile("./src/assets/upload.csv");
-
-
-// function getRandomInt(min, max) {
-//   min = Math.ceil(min);
-// 	max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-// }
-
-// function makeRandomName(letters) {
-// 	let name = "";
-// 	for (let i = 0; i < 6; i++) {
-// 		const letterIndex = getRandomInt(0, 26);
-// 		name = 	name + letters[letterIndex]
-// 	}
-
-// 	return name;
-// }
-
-
 
 db.sequelize
   .sync({ force: true })
